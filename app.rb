b3f60a6ea3dac20078400c2252d2650b75a13aa3
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-05-12 16:52:28 -0300

    Add main and app structure

---
+++ app.rb
@@ -0,0 +1,26 @@
+class App
+  def initialize
+    @persons = []
+    @books = []
+  end
+
+  attr_accessor :persons, :books
+
+  def list_all_books
+  end
+
+  def list_all_persons
+  end
+
+  def create_person
+  end
+
+  def create_book
+  end
+
+  def create_rental
+  end
+
+  def list_rentals
+  end
+end
